31fdf41e1c55fa40407fe755ff27e1defa83328e
luisortiz1211 <luis.ortiz02@epn.edu.ec>
2021-08-06 00:30:02 -0500

    creacion de calculadora react en ionic

---
+++ src/components/Display.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "../theme/Display.css";
+
+class Display extends Component {
+  render() {
+    return <div className="Display">{this.props.data}</div>;
+  }
+}
+
+export default Display;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import Calculator from '../components/Calculator';
 import './Home.css';
 
 const Home: React.FC = () => {
@@ -7,16 +7,12 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Calculadora</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+      <IonContent fullscreen >
+           <Calculator />
+       
       </IonContent>
     </IonPage>
   );
